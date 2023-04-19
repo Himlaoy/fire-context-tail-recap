51f08f8928cf66cee569cfa00348bbf36b8a6886
himaloyash <himaloyashis98@gmail.com>
2023-04-19 18:05:22 +0600

    context added

--- src/main.jsx
+++ src/main.jsx
@@ -10,28 +10,31 @@ import Home from './components/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Main from './components/Main/Main';
+import AuthProviders from './Providers/AuthProviders';
 
 const router = createBrowserRouter([{
-  path:'/',
-  element:<Main></Main>,
-  children:[
+  path: '/',
+  element: <Main></Main>,
+  children: [
     {
-      path:'/',
-      element:<Home></Home>
+      path: '/',
+      element: <Home></Home>
     },
     {
-      path:'/login',
-      element:<Login></Login>,
+      path: '/login',
+      element: <Login></Login>,
     },
     {
-      path:'/register',
-      element:<Register></Register>,
+      path: '/register',
+      element: <Register></Register>,
     }
   ]
 }])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProviders>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProviders>
   </React.StrictMode>,
 )
---
+++ src/step.js
@@ -0,0 +1,8 @@
+/**
+ *  context api
+ * 1. create context api(export)
+ * 2. create a provider
+ * 3. set context value
+ * 4. to access the value use useContext hook .
+ * 
+ * */ 
