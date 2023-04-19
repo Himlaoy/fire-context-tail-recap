ff981a7f8f369f516084f4234eac3cb4072a788a
himaloyash <himaloyashis98@gmail.com>
2023-04-19 16:28:09 +0600

    firebase added

---
+++ src/fire/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy17_0RoFEw_ptijxSJu5sWogGtvaJ0PU",
+  authDomain: "fire-context-taill-recap.firebaseapp.com",
+  projectId: "fire-context-taill-recap",
+  storageBucket: "fire-context-taill-recap.appspot.com",
+  messagingSenderId: "131306522779",
+  appId: "1:131306522779:web:4dd14240e1b3876160de3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
